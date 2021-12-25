eabcd663d7cd29d01a4cb5a33c97e158b498707a
Asilvorcarp <hs.fish1213@gmail.com>
2021-12-25 16:54:07 +0800

    上链

--- pages/creating.jsx
+++ pages/creating.jsx
@@ -13,25 +13,49 @@ export default function CreatePage() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className={styles.boxDiv}>
-        <button className={styles.selectBox}></button>
-        <p className={styles.upload_reminder}></p>
-        <button className={styles.selectBox}></button>
-        <br/>
+      <div className={styles.divColParent}>
+        <div className={styles.boxDiv, styles.divColLeft}>
+          <button className={styles.selectBox}></button>
+          <p className={styles.upload_reminder}></p>
+          <button className={styles.selectBox}></button>
+          <br/>
 
-        {/* //todo: RESELECT */}
-        <Link href="/todo RESELECT">
-          <button className={styles.reselect} >RESELECT</button>
-        </Link>
-      </div>
+          {/* //todo: RESELECT */}
+          <Link href="/todo RESELECT">
+            <button className={styles.reselect} >RESELECT</button>
+            {/* //todo 返回的图标 */}
+          </Link>
+        </div>
 
-      <div className={styles.divRight}>
-        <button className={styles.tags}></button><br/><br/>
-        <input className={styles.nameInput}></input><br/><br/>
-        <input className={styles.tagsInput}></input><br/><br/>
-        <input className={styles.priceInput}></input><br/><br/>
-        <input className={styles.durationInput}></input><br/><br/>
-        <input className={styles.descriptionInput}></input><br/><br/>
+        <div className={styles.divColRight}>
+          <div className='columns-2'>
+            <Image
+              src={require('/public/outputImg.png')} // Route of the image file
+              height={452} // Desired size with correct aspect ratio
+              width={452} // Desired size with correct aspect ratio
+              alt="Output Img"
+            />
+            <div>
+              <br/>
+              {/* <button className={styles.tags}></button><br/><br/> */}
+              <p>Name</p>
+              <input className={styles.nameInput}></input><br/><br/>
+              <p>Tags</p>
+              <input className={styles.tagsInput}></input><br/><br/>
+              <p>Price</p>
+              <input className={styles.priceInput}></input><br/><br/>
+              <p>Fees</p>
+              <p>Duration</p>
+              <input className={styles.durationInput}></input><br/><br/>
+            </div>
+          </div>
+          <p>Description</p>
+          <input className={styles.descriptionInput}></input><br/><br/>
+
+          <button className={styles.listOnBazaar} onClick={()=>{
+            // todo 生成的图片上链生成NFT 并且加入市场(todo later)
+          }}>LIST ON BAZAAR</button>
+        </div>
       </div>
 
     </>
